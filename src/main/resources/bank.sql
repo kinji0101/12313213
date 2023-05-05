0c84c38ace0fd8661f1e449e035f81ba2fb49b0f
MSI\kinji <kidrrrooobbb@gmail.com>
2023-05-05 16:58:55 +0800

    sb

--- src/main/resources/bank.sql
+++ src/main/resources/bank.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `bank_system` (
   `card` varchar(50) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `emaill` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
   `address` varchar(45) DEFAULT NULL,
   `phoneNumbe` int DEFAULT NULL,
   `deposit` int DEFAULT NULL,
