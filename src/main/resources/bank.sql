75947b48fbe68945c444be9676902d5e5b7825c8
kinji0101 <128030768+kinji0101@users.noreply.github.com>
2023-05-06 15:04:13 +0800

    Update bank.sql

--- src/main/resources/bank.sql
+++ src/main/resources/bank.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS `bank_system` (
+CREATE TABLE IF NOT EXISTS`bank_system` (
   `card` varchar(50) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
+  `emaill` varchar(45) NOT NULL,
   `address` varchar(45) DEFAULT NULL,
   `phoneNumbe` int DEFAULT NULL,
   `deposit` int DEFAULT NULL,
@@ -10,5 +10,4 @@ CREATE TABLE IF NOT EXISTS `bank_system` (
   `loanRate` int DEFAULT NULL,
   `offer` int DEFAULT NULL,
   PRIMARY KEY (`card`)
-) 
-) 
+)
